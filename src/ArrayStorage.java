b4c9815ee860a8fbd5bc64b5e3166fb69fc5c09f
Nurzat Smagulov <nsmagulov@lt-m.ru>
2021-09-22 00:38:09 +0300

    Add condition

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,25 +9,20 @@ public class ArrayStorage {
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        int i = 0;
         if(r.uuid != null) {
-            do {
-                storage[i] = r;
-                i++;
-            } while (storage[size] == null);
+            storage[size] = r;
+            size++;
         }
-        size = i;
     }
 
     Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 return storage[i];
-            } else if(storage[size] == null) {
-                System.out.println(uuid + "uuid Не найден");
             }
         }
         return null;
@@ -37,8 +32,9 @@ public class ArrayStorage {
         for(int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 storage[i] = null;
-                if (storage.length - 1 - i >= 0) 
-                    System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - i);
+                if (size - 1 - i >= 0)
+                    System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
+                    size--;
             } else {
                 System.out.println("Резюме не было найдено!");
            }
@@ -49,7 +45,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
