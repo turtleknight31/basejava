563f772e6113ea0dc222f603422dba35bdcdcac8
Nurzat Smagulov <nsmagulov@lt-m.ru>
2021-10-06 12:20:05 +0300

    HW2 add package

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,3 +1,7 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
 /**
  * Array based storage for Resumes
@@ -6,60 +10,60 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         boolean existResume = true;
         for (int i = 0; i < size; i++) {
-            if(r.uuid.equals(storage[i].uuid)) {
+            if (r.getUuid().equals(storage[i].getUuid())) {
                 System.out.println("Данное резюме уже существует в базе!");
                 existResume = false;
             }
         }
-        if(existResume && r.uuid != null && size < 10000) {
+        if (existResume && r.getUuid() != null && size < 10000) {
             storage[size] = r;
             size++;
-        } else if(size > 9999) {
+        } else if (size > 9999) {
             System.out.println("Резюме переполнилась, добавить новое резюме не получится!");
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
-        for(int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
                 storage[i] = storage[size - 1];
                 storage[i] = null;
                 size--;
-           }
+            }
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
-    void update(Resume resume)  {
-        for(int i = 0; i < size; i++) {
-            if (resume.uuid.equals(storage[i].uuid)) {
+    public void update(Resume resume) {
+        for (int i = 0; i < size; i++) {
+            if (resume.getUuid().equals(storage[i].getUuid())) {
                 storage[i] = resume;
             }
         }
