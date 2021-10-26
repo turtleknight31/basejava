fc28f324bdb6f255683e05f7a172ac5464d50ef6
Nurzat Smagulov <nsmagulov@lt-m.ru>
2021-10-26 15:24:33 +0300

    HW-4 reflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,16 +3,17 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException{
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
-        System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        //вызвать метод тострин через отражения invoke r tostring via reflection
-        System.out.println(r);
+        Method method = r.getClass().getDeclaredMethod("toString");
+        System.out.println(method.invoke(r, null));
     }
 }
